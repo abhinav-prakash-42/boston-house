f547a44c6a26ebff33d50ec9d4b74f7113cb1e5d
Abhinav Prakash <abhinav1842prakash@gmail.com>
2025-08-03 22:58:18 +0530

    This is for simple html and predict api

--- app.py
+++ app.py
@@ -30,5 +30,13 @@ def predict_api():
 
     return jsonify(prediction[0])
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output = model.predict(final_input)[0]
+    return render_template ('home.html', prediction_text='Predicted Price: ${:,.2f}'.format(output))
+
 if __name__ == '__main__':
     app.run(debug=True)
